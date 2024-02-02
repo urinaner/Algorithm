552a26b8fe8cfd97a454b4f6b71bf08b6a7d00a6
장영재 <urinaner@naver.com>
2024-02-02 16:14:24 +0900

    2/2 - 16

--- 덱.py
+++ 덱.py
@@ -1,6 +1,8 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
 n = int(input())
 
-from collections import deque
 dq = deque([])
 
 for i in range(n):
@@ -11,14 +13,14 @@ for i in range(n):
         dq.appendleft(int(com[1]))
     elif com[0] == "pop_back":
         if dq:
-            print(-1)
-        else:
             print(dq.pop())
+        else:
+            print(-1)
     elif com[0] == "pop_front":
         if dq:
-            print(-1)
-        else:
             print(dq.popleft())
+        else:
+            print(-1)
     elif com[0] == "size":
         print(len(dq))
     elif com[0] == "empty":
