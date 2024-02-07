85ef2f2c09d19a71c6b24954e3fad28b90b77cc2
urinaner <urinaner@naver.com>
2024-02-07 22:41:39 +0900

    2-7

---
+++ 주사위세개.py
@@ -0,0 +1,12 @@
+x, y, z = map(int, input().split())
+
+if x==y and y==z and x==z:
+    print(10000+x*1000)
+elif x==y:
+    print(1000+x*100)
+elif z==y:
+    print(1000+y*100)
+elif x==z:
+    print(1000+x*100)
+else:
+    print(max(x,y,z)*100)
