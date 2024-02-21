204c204facc487308c150acdfecb507d6e09b8e5
장영재 <urinaner@naver.com>
2024-02-21 17:14:36 +0900

    2/21 17:00

--- 수들의조합.py
+++ 수들의조합.py
@@ -1,1 +1,21 @@
 n, k = map(int, input().split())
+
+arr = list(map(int, input().split()))
+m = int(input())
+idx = [0] * (k+1)
+
+
+def DFS(L):
+    if L == k:
+        for i in range(k): 
+            print(idx[i], end=" ")
+        print()
+        return
+    else:
+        for i in range(k):
+            if idx[i] == 0:
+                idx[i] = arr[i]
+                DFS(L+1)
+                idx[i] = 0
+ 
+DFS(0)
