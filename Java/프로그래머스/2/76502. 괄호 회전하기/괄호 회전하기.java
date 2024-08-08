023fc3fd80361772285387a69de8192bad8cd271
장영재 <urinaner@naver.com>
2024-08-08 18:32:47 +0900

    [level 2] Title: 괄호 회전하기, Time: 51.09 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        HashMap<Character, Character> map = new HashMap<>();
+        
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+        
+        int n = s.length();
+        s+=s;
+        int answer = 0;
+        
+        A:for(int i=0; i<n; i++){
+            ArrayDeque<Character> stack = new ArrayDeque<>();
+            
+            for(int j=i; j<i+n; j++){
+                char c = s.charAt(j);
+                
+                if(!map.containsKey(c)){
+                    stack.push(c);
+                }else{
+                    if(stack.isEmpty() || !stack.pop().equals(map.get(c))){
+                        continue A;
+                    }
+                }   
+            }
+            if(stack.isEmpty()){
+                    answer++;
+                }
+        }
+        return answer;
+    }
+}
