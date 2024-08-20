da5a9dbc2effbe40aceef777dae12792a7a137d4
장영재 <urinaner@naver.com>
2024-08-20 11:16:57 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int service = n / 10;
+        
+        answer = n * 12000 + (k-service) * 2000;
+        return answer;
+    }
+}
