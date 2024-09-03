8da15d1335cf76c34caf126a43d4f7d9a53c1e9c
장영재 <urinaner@naver.com>
2024-09-04 01:25:53 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.26 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String []arr = my_string.replaceAll("[a-zA-z]", " ").split(" ");
+        
+        for(String s : arr){
+            if(!s.equals("")){
+                answer += Integer.parseInt(s);
+            }
+        }
+        return answer;
+    }
+}
