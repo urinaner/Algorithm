80bc95000ce79e565cf01e999e4e3cba1f8e1862
장영재 <urinaner@naver.com>
2025-01-02 22:56:03 +0900

    [Bronze V] Title: 등록, Time: 92 ms, Memory: 13848 KB -BaekjoonHub

---
+++ Java/백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args)  {
+        System.out.println(273);
+        System.out.println("urinaner");
+    }
+}
