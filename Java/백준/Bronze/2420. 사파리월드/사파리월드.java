85bb5df9cd9087ed4df2eb61f4b43844edab6792
장영재 <urinaner@naver.com>
2025-01-05 23:52:03 +0900

    [Bronze V] Title: 사파리월드, Time: 104 ms, Memory: 14080 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long n = Integer.parseInt(st.nextToken());
+        long m = Integer.parseInt(st.nextToken());
+        long answer = n-m;
+        if(answer>=0){
+            System.out.println(answer);
+        }else {
+            System.out.println(-answer);
+        }
+    }
+}
