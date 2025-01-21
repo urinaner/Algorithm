3b05684dcec82458c3c3a1894c58d03b858e3926
장영재 <urinaner@naver.com>
2025-01-21 14:35:34 +0900

    [Silver IV] Title: 설탕 배달, Time: 112 ms, Memory: 14352 KB -BaekjoonHub

--- Java/백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ Java/백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,30 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int salt = Integer.parseInt(st.nextToken());
-        int answer = 0;
-        if(salt % 5 == 0){
-            System.out.println(salt / 5);
-            System.exit(0);
-        }
-        if(salt == 4 || salt == 7){
-            System.out.println(-1);
-            System.exit(0);
-        }
 
-        if((salt % 5 == 1) || (salt % 5 == 3)){
-           answer = salt / 5 + 1;
-        } else if (salt % 5 == 4 || salt % 5 == 2) {
-            answer = salt / 5 + 2;
-        }
+        int salt = Integer.parseInt(br.readLine());
 
-        System.out.println(answer);
+        int []dp = new int[salt+1];
+        Arrays.fill(dp, -1);
+
+        if (salt >= 3) dp[3] = 1;
+        if (salt >= 5) dp[5] = 1;
+
+        for(int i=6; i<=salt; i++){
+            if(dp[i-3] != -1){
+                dp[i] = dp[i-3] + 1;
+            }
+            if(dp[i-5] != -1){
+                dp[i] = dp[i-5] + 1;
+            }
+        }
+        System.out.println(dp[salt]);
     }
 
 }
