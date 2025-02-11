a7dd7a027298e52f342e85fdfe6f81f3545b6ffc
장영재 <urinaner@naver.com>
2025-02-11 18:46:59 +0900

    [Silver IV] Title: 수 찾기, Time: 992 ms, Memory: 52792 KB -BaekjoonHub

--- Java/백준/Silver/1920. 수 찾기/수 찾기.java
+++ Java/백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,26 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
+import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static int N, M;
+    static int []arr;
+    static int []numbers;
+
+
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        HashSet<Integer> set = new HashSet<>();
-        int n = Integer.parseInt(br.readLine());
+        initialize(br);
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++){
-            set.add(Integer.parseInt(st.nextToken()));
-        }
+        Arrays.sort(arr);
 
-        int m = Integer.parseInt(br.readLine());
-
-        st = new StringTokenizer(br.readLine());
-        for(int j=0; j<m; j++){
-            if(set.contains(Integer.parseInt(st.nextToken()))){
+        for(int i=0; i<M; i++){
+            if(isExistNumber(numbers[i])){
                 System.out.println(1);
             }else{
                 System.out.println(0);
@@ -29,5 +26,41 @@ public class Main {
 
     }
 
+    static void initialize(BufferedReader br) throws IOException {
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
 
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        M = Integer.parseInt(br.readLine());
+        numbers = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<M; i++){
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    static boolean isExistNumber(int num){
+        int start = 0;
+        int end = N-1;
+
+        while (start <= end){
+            int mid = (start + end) / 2;
+            if(arr[mid] == num)
+                return true;
+
+            if(arr[mid] > num){
+                end = mid-1;
+            } else if (arr[mid] < num) {
+                start = mid + 1;
+            }
+        }
+        return false;
+    }
+
+    
 }
