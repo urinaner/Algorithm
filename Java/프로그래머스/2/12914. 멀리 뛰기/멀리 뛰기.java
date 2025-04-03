217a8fa84034aef5a84626ba1bd748e11addad5c
장영재 <urinaner@naver.com>
2025-04-03 20:04:32 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.21 ms, Memory: 75.4 MB -BaekjoonHub

--- Java/프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ Java/프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,16 +1,6 @@
 class Solution {
     public long solution(int n) {
         long answer = 0;
-        int []dp = new int[2001];
-        
-        dp[1] = 1;
-        dp[2] = 2;
-        if(n == 1 || n == 2){
-            return dp[n];
-        }
-        for(int i=3; i<=n; i++){
-            dp[i] = dp[i-1] % 1234567 + dp[i-2] % 1234567;
-        }
-        return dp[n] % 1234567;
+        return answer;
     }
 }
