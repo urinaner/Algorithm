18a09713a9d98354184473b851fee9d8725f5498
장영재 <urinaner@naver.com>
2026-01-18 17:51:12 +0900

    [level 1] Title: 폰켓몬, Time: 6.68 ms, Memory: 84 MB -BaekjoonHub

--- Java/프로그래머스/1/1845. 폰켓몬/README.md
+++ Java/프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
 
 ### 성능 요약
 
-메모리: 74.7 MB, 시간: 9.59 ms
+메모리: 84 MB, 시간: 6.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 24일 14:05:03
+2026년 01월 18일 17:51:10
 
 ### 문제 설명
 
--- Java/프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ Java/프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,15 +1,20 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        int numLen = nums.length;
-        for(int number : nums){
-            map.put(number, map.getOrDefault(number, 0) + 1);
+        Set<Integer> set = new HashSet<>();
+        //map에 포켓몬 추가
+        for(Integer num : nums){
+            set.add(num);
         }
         
-        answer = Math.min(numLen / 2, map.size());
+        if(set.size() <= nums.length / 2){
+            answer = set.size();
+        }else {
+            answer = nums.length / 2;
+        }
+        //포켓몬 길이가 / 2 가 포켓몬 고유 개수보다 작으면 고유개수 반환 아니면 포켓몬 길이/2 반환
         
         return answer;
     }
