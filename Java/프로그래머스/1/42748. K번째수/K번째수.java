529e42b73b602076f7a370fa70ecf99e3d11e6b8
장영재 <urinaner@naver.com>
2026-01-28 22:27:45 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 72.4 MB -BaekjoonHub

--- Java/프로그래머스/1/42748. K번째수/K번째수.java
+++ Java/프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,18 +1,15 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        
-        
         for(int i=0; i<commands.length; i++){
-            int arrLen = commands[i][1] - commands[i][0] + 1;
-            int []tmp = new int[arrLen];
-            
-            for(int j=0; j<arrLen; j++){
-                tmp[j] = array[commands[i][0] - 1 + j];
+            List<Integer> list = new ArrayList<>();
+            for(int j=commands[i][0]; j<=commands[i][1]; j++){
+                list.add(array[j-1]);
             }
-            Arrays.sort(tmp);
-            answer[i] = tmp[commands[i][2] -1];
+            Collections.sort(list);
+            answer[i] = list.get(commands[i][2]-1);
         }
         return answer;
     }
